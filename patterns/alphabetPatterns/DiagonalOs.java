6d2698d91a373d737a2502c39aa45fc57d1854c8
randomUser00 <75338386+randomUser00@users.noreply.github.com>
2020-12-02 09:35:30 -0500

    Create DiagonalOs.java

---
+++ patterns/alphabetPatterns/DiagonalOs.java
@@ -0,0 +1,11 @@
+public class DiagonalOs {
+    public static void main(String[] args) {
+        // Write your code here
+        String spaces = "";
+
+        for(int row = 1; row < 11; row++) {
+            System.out.println(spaces+"O");
+            spaces += " ";
+        }
+    }
+}
