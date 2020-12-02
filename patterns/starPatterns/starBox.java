44f31db4704dfcbea5211395dfaa852c5992b7b4
randomUser00 <75338386+randomUser00@users.noreply.github.com>
2020-12-02 10:11:54 -0500

    Update starBox.java

--- patterns/starPatterns/starBox.java
+++ patterns/starPatterns/starBox.java
@@ -1,26 +1,36 @@
 // Prints a box of stars 
 // number of stars is n
-
-public static void starBox(int n)
+public class starBox
 {
-  for (int i = 0; i < n: ++i)
-  {
-    System.out.print("*");
-  }
-  System.out.println();
-  for (int i = 0; i < (n-2); ++i)
-  {
-    System.out.print("*");
-    for (int j = 0; j < (n-2); ++j)
+    public static void main(String [] args)
     {
-      System.out.print(" ");
+        starBox(5);
+        starBox(10);
     }
-     System.out.print("*");
-     System.out.println();
-  }
-  for (int i = 0; i < n; ++i)
-  {
-    System.out.print("*");
-  }
-  System.out.println();
+
+    public static void starBox(int n)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+        
+        for (int i = 0; i < (n-2); ++i)
+        {
+            System.out.print("*");
+            for (int j = 0; j < (n-2); ++j)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+        
+        for (int i = 0; i < n; ++i)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
