69e18687239829a4c925a09505d29d98f72d8b40
randomUser00 <75338386+randomUser00@users.noreply.github.com>
2020-12-02 10:25:06 -0500

    Create DiagonalNum.java

---
+++ patterns/numPatterns/DiagonalNum.java
@@ -0,0 +1,21 @@
+//    1
+//   2
+//  3
+// 4
+//5
+
+// You can change the spaces to dots
+public class DiagonalNum
+{
+    public static void main(String [] args)
+    {
+        for(int i = 1; i <=5; ++i)
+        {
+            for (int j = 1; j <= (5-i); ++j)
+            {
+                System.out.print(" ");
+            }
+            System.out.println(i);
+        }
+    }
+}
