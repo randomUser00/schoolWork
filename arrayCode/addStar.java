7eea822694209e0d44c36116eff3b7ed537144ab
randomUser00 <75338386+randomUser00@users.noreply.github.com>
2020-12-07 21:36:17 -0500

    Create addStar.java

---
+++ arrayCode/addStar.java
@@ -0,0 +1,28 @@
+// adds a star after eaxh element of a string array
+
+public class addStar
+{
+    public static void main(String [] args)
+    {
+        String [] a = {"once", "two", "three", "four"};
+
+        String [] a1 = addStar(a);
+
+        for(String val : a1)
+        {
+            System.out.print(val+" ");
+        }
+    }
+
+    public static String[] addStar(String [] b)
+    {
+        String [] b1 = new String[b.length * 2];
+
+        for(int i = 0; i < b.length; ++i)
+        {
+            b1[2 * i] = b[i];
+            b1[(2 * i) + 1] = "*";
+        }
+        return b1;
+    }
+}
